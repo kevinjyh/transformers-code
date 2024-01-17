1ae7c41bd08d2cdb41e880367de7641a86370b9e
Kevin <kevinjyh@gmail.com>
2024-01-17 18:39:49 +0800

    保留demo.py的兩種pipeline；hosts做備份

--- 01-Getting Started/01-introduction/demo.py
+++ 01-Getting Started/01-introduction/demo.py
@@ -4,4 +4,5 @@ import gradio as gr
 from transformers import pipeline
 # 通过Interface加载pipeline并启动阅读理解服务
 # 如果无法通过这种方式加载，可以采用离线加载的方式
+# gr.Interface.from_pipeline(pipeline("text-classification", model="uer/roberta-base-finetuned-dianping-chinese")).launch()
 gr.Interface.from_pipeline(pipeline("question-answering", model="uer/roberta-base-chinese-extractive-qa")).launch()
